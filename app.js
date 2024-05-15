8b07ef25b84cb9ac1a5fa67d36cec6e6d41fee02
Bishalhamilton <Sarkar@hamiltonkw.com>
2024-05-15 11:56:43 +0530

    update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { resume_mail } = require(".");
 
 const app = express();
 
-resume_mail(["vidit.patel@embrill.com"]);
+resume_mail(["hr@trickywebsolutions.com"]);
 
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
